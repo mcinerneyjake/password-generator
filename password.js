22b0270dd2643964e464da3e480f0f9220873a6e
jakemcinerney <97058450+mcinerneyjake@users.noreply.github.com>
2022-01-29 17:14:54 -0600

    Update password.js

--- password.js
+++ password.js
@@ -15,9 +15,46 @@ btn.addEventListener("click", function randomPassword() {
     retVal += charset.charAt(Math.floor(Math.random() * n));
   }
   if (btn.clicked === true) {
-    textBoxInput.textContent = console.log(retVal);
+    textBoxInput.textContent = "Error";
   } else {
     textBoxInput.textContent = retVal.valueOf("");
   }
   return retVal.valueOf("");
+});
+
+const nums = document.getElementById("nums");
+const uppers = document.getElementById("uppers");
+const lowers = document.getElementById("lowers");
+const symbols = document.getElementById("symbols");
+
+nums.addEventListener("change", function () {
+  if (nums.checked === true) {
+    textBoxInput.textContent = 1234567890;
+  } else {
+    textBoxInput.textContent = "Click 'Generate' button again.";
+  }
+});
+
+uppers.addEventListener("change", function () {
+  if (uppers.checked === true) {
+    textBoxInput.textContent = "UPPERCASE";
+  } else {
+    textBoxInput.textContent = "Click 'Generate' button again.";
+  }
+});
+
+lowers.addEventListener("change", function () {
+  if (lowers.checked === true) {
+    textBoxInput.textContent = "lowercase";
+  } else {
+    textBoxInput.textContent = "Click 'Generate' button again.";
+  }
+});
+
+symbols.addEventListener("change", function () {
+  if (symbols.checked === true) {
+    textBoxInput.textContent = "!@#$%^&*";
+  } else {
+    textBoxInput.textContent = "Click 'Generate' button again.";
+  }
 });
